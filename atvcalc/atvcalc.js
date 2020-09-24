bd33724643a1d56b16961d6b947980c4e317b05a
zMaarrco <gamesmrco@gmail.com>
2020-09-23 21:23:21 -0300

    Versão Final

--- atvcalc/atvcalc.js
+++ atvcalc/atvcalc.js
@@ -1,5 +1,14 @@
 
-function botao( num ) {
-    var salvo = document.calc.visor.value;
-    document.calc.visor.value = salvo + num;
+var valor;
+var resultado;
+
+function botao(num) {
+    valor = document.calc.visor.value += num;
+}
+function limpar(){
+    document.calc.visor.value = '';
+}
+function calcular(){
+    resultado = eval(valor);
+    document.calc.visor.value = resultado;
 }
